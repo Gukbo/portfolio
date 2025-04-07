3b41274217911eef97f174b2384cf1f37e77436c
Gukbo <2onesound@naver.com>
2025-04-07 20:43:20 +0900

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import Home from "./components/Home";
+import Project from "./components/Project";
+import About from "./components/About";
+
+function App() {
+  return (
+    <>
+      <div>
+        <Home />
+        <Project />
+        <About />
+      </div>
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Project.jsx
@@ -0,0 +1,9 @@
+const Project = () => {
+  return (
+    <>
+      <p>Project</p>
+    </>
+  );
+};
+
+export default Project;
